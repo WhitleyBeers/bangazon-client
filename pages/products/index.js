11e87df5f0f9dc380d85d3fc62de2940e6274d98
Whitley Beers <whitleybeers@gmail.com>
2023-07-27 20:05:26 -0500

    all products page completed

---
+++ pages/products/index.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { getAllProducts } from '../../api/productData';
+import ProductCard from '../../components/ProductCards';
+
+export default function AllProductsPage() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = () => {
+    getAllProducts().then(setProducts);
+  };
+
+  useEffect(() => {
+    getProducts();
+  });
+
+  return (
+    <div
+      className="my-2 d-flex justify-content-center flex-wrap"
+    >
+      {products.map((product) => (
+        <ProductCard productObj={product} key={product.id} />
+      ))}
+    </div>
+  );
+}
