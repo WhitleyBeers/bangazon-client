08c769ef262cc059336f7eb5c65c75ffe7cfe377
Whitley Beers <whitleybeers@gmail.com>
2023-07-29 09:53:10 -0500

    order history page now shows user sales as well

--- pages/orders/index.js
+++ pages/orders/index.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../utils/context/authContext';
-import { getUserOrders } from '../../api/orderData';
+import { getSellerOrders, getUserOrders } from '../../api/orderData';
 import OrderCard from '../../components/OrderCard';
 
 export default function OrderHistoryPage() {
   const { user } = useAuth();
   const [orders, setOrders] = useState([]);
+  const [sales, setSales] = useState([]);
 
   const getMyOrders = () => {
     getUserOrders(user.id).then(setOrders);
+    getSellerOrders(user.id).then(setSales);
   };
 
   useEffect(() => {
@@ -17,10 +19,19 @@ export default function OrderHistoryPage() {
   }, user);
 
   return (
-    <div className="text-center d-flex justify-content-center flex-column">
-      {orders.map((order) => (
-        <OrderCard key={order.id} orderObj={order} />
-      ))}
-    </div>
+    <>
+      <div className="text-center d-flex justify-content-center flex-column mt-3">
+        <h3>My Orders</h3>
+        {orders.map((order) => (
+          <OrderCard key={order.id} orderObj={order} />
+        ))}
+      </div>
+      <div className="text-center d-flex justify-content-center flex-column mt-3">
+        <h3>My Sales</h3>
+        {sales.map((sale) => (
+          <OrderCard key={sale.id} orderObj={sale} />
+        ))}
+      </div>
+    </>
   );
 }
